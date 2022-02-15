a44429984b93eb33e3cea1bbb39dda7d63d89e08
gisore <leegisore@gmail.com>
2022-02-16 00:27:37 +0300

    Complete 1st practise set

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created by gisore on 2/15/22
+ */
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+
+}
---
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * created by gisore on 2/15/22
+ */
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
